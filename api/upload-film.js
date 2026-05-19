23805633fa2bec510fe9894f16f857dde300c31d
Braden White <bradenwhite2024@gmail.com>
2026-05-19 15:28:28 -0400

    fix api function commonjs format

--- api/upload-film.js
+++ api/upload-film.js
@@ -1,5 +1,5 @@
-import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3'
-import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3')
+const { getSignedUrl } = require('@aws-sdk/s3-request-presigner')
 
 const R2 = new S3Client({
   region: 'auto',
@@ -10,15 +10,22 @@ const R2 = new S3Client({
   },
 })
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+  if (req.method === 'OPTIONS') return res.status(200).end()
   if (req.method !== 'POST') return res.status(405).end()
+
   const { teamId, gameId } = req.body
   const key = `${teamId}/${gameId}/film.mp4`
+
   const command = new PutObjectCommand({
     Bucket: process.env.VITE_R2_BUCKET,
     Key: key,
     ContentType: 'video/mp4',
   })
+
   const url = await getSignedUrl(R2, command, { expiresIn: 3600 })
-  res.json({ url, key })
+  return res.status(200).json({ url, key })
 }
